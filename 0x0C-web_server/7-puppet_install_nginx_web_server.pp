6949ab0c6d785388e09b69f666ab305a6b9096d5
thymac <thywillberdone@gmail.com>
2023-05-09 01:37:33 -0700

    revised

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,54 +1,40 @@
-class nginx_hello_world {
-  package { 'nginx':
-    ensure => installed,
-  }
+#7-puppet_install_nginx_web_server.pp
 
-  service { 'nginx':
-    ensure => running,
-    enable => true,
-  }
-
-  file { '/var/www/html/index.html':
-    ensure  => file,
-    content => "Hello World!\n",
-  }
-
-  file { '/etc/nginx/sites-available/default':
-    ensure  => file,
-    content => "
-      server {
-        listen 80 default_server;
-        listen [::]:80 default_server;
-        return 301 \"https://$server_name$request_uri\";
-      }
-
-      server {
-        listen 443 ssl default_server;
-        listen [::]:443 ssl default_server;
-        ssl_certificate /etc/ssl/certs/ssl-cert-snakeoil.pem;
-        ssl_certificate_key /etc/ssl/private/ssl-cert-snakeoil.key;
-        root /var/www/html;
-        index index.html;
-        server_name _;
-        location /redirect_me {
-          return 301 \"https://$server_name/\";
-        }
-        location / {
-          try_files $uri $uri/ =404;
-        }
-      }
-    ",
-  }
-
-  file { '/etc/nginx/sites-enabled/default':
-    ensure => 'link',
-    target => '/etc/nginx/sites-available/default',
-  }
-
-  exec { 'nginx-reload':
-    command     => '/usr/sbin/service nginx reload',
-    refreshonly => true,
-  }
+#Install Nginx package
+package { 'nginx':
+  ensure => installed,
 }
 
-include nginx_hello_world
+#Configure Nginx service
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => Package['nginx'],
+}
+
+#Configure Nginx virtual host
+file { '/etc/nginx/sites-available/default':
+  ensure => file,
+  content => "
+    server {
+      listen 80;
+      server_name _;
+      root /var/www/html;
+      index index.html;
+
+      location / {
+        return 301 http://\$host/redirect_me;
+      }
+
+      location /redirect_me {
+        return 301 http://\$host/hello;
+      }
+
+      location /hello {
+        echo 'Hello World!';
+      }
+    }
+  ",
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
