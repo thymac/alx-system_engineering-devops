7b3ea82262b1f82278aee2268cc3776d66760443
thymac <thywillberdone@gmail.com>
2023-05-25 13:23:30 -0700

    revised

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,41 +1,59 @@
 #!/usr/bin/python3
-"""
-Python script that, using a REST API, retrieves and displays information
-about the TODO list progress of a given employee ID.
-"""
-
+"""script that fetches info about a given employee's ID using an api"""
+import json
 import requests
-from sys import argv
+import sys
 
+
+base_url = 'https://jsonplaceholder.typicode.com'
 
 if __name__ == "__main__":
-    if len(argv) < 2:
-        exit()
 
-    employee_id = argv[1]
-    base_url = "https://jsonplaceholder.typicode.com"
+    user_id = sys.argv[1]
 
-    # Get the employee's information
-    user_url = f"{base_url}/users/{employee_id}"
+    # get user info e.g https://jsonplaceholder.typicode.com/users/1/
+    user_url = '{}/users?id={}'.format(base_url, user_id)
+    # print("user url is: {}".format(user_url))
+
+    # get info from api
     response = requests.get(user_url)
-    user_data = response.json()
-    employee_username = user_data.get("username")
+    # pull data from api
+    data = response.text
+    # parse the data into JSON format
+    data = json.loads(data)
+    # extract user data, in this case, name of employee
+    name = data[0].get('name')
+    # print("id is: {}".format(user_id))
+    # print("name is: {}".format(name))
 
-    # Get the employee's TODO tasks
-    todo_url = f"{base_url}/todos?userId={employee_id}"
-    response = requests.get(todo_url)
-    todo_data = response.json()
+    # get user info about todo tasks
+    # e.g https://jsonplaceholder.typicode.com/users/1/todos
+    tasks_url = '{}/todos?userId={}'.format(base_url, user_id)
+    # print("tasks url is: {}".format(tasks_url))
 
-    # Count the number of completed tasks and total tasks
-    completed_tasks = [task for task in todo_data if task.get("completed")]
-    total_tasks = len(todo_data)
+    # get info from api
+    response = requests.get(tasks_url)
+    # pull data from api
+    tasks = response.text
+    # parse the data into JSON format
+    tasks = json.loads(tasks)
 
-    # Display the employee TODO list progress
-    print(f"Employee {employee_username} is done with tasks "
-          f"({len(completed_tasks)}/{total_tasks}):")
+    # initialize completed count as 0 and find total number of tasks
+    completed = 0
+    total_tasks = len(tasks)
 
-    # Display the title of completed tasks
+    # initialize empty list for completed tasks
+    completed_tasks = []
+    # loop through tasks counting number of completed tasks
+    for task in tasks:
+
+        if task.get('completed'):
+            # print("The tasks are: {}\n".format(task))
+            completed_tasks.append(task)
+            completed += 1
+
+    # print the output in the required format
+    print("Employee {} is done with tasks({}/{}):"
+          .format(name, completed, total_tasks))
     for task in completed_tasks:
-        task_title = task.get("title")
-        print(f"\t{task_title}")
-
+        print("\t {}".format(task.get('title')))
