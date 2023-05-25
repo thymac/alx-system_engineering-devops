cf3bb31074d1c1745fc4ffffb3e4a8da3dde0c1f
thymac <thywillberdone@gmail.com>
2023-05-25 13:30:43 -0700

    revised

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,35 +10,34 @@ import sys
 base_url = 'https://jsonplaceholder.typicode.com'
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: python3 script.py [employee_id]")
-        sys.exit(1)
+    user_id = sys.argv[1]
 
-    employee_id = sys.argv[1]
-
-    # Retrieve user information
-    user_url = f'{base_url}/users?id={employee_id}'
+    # Get user info
+    user_url = f'{base_url}/users?id={user_id}'
     response = requests.get(user_url)
     data = response.json()
 
+    # Check if user exists
     if not data:
-        print(f"No user found with ID: {employee_id}")
+        print(f"No user found with ID: {user_id}")
         sys.exit(1)
 
-    user = data[0]
-    employee_name = user.get('name')
+    name = data[0]['name']
 
-    # Retrieve user's TODO tasks
-    tasks_url = f'{base_url}/todos?userId={employee_id}'
+    # Get user's TODO tasks
+    tasks_url = f'{base_url}/todos?userId={user_id}'
     response = requests.get(tasks_url)
     tasks = response.json()
 
-    # Count the number of completed tasks and total tasks
-    completed_tasks = [task for task in tasks if task.get('completed')]
-    completed_count = len(completed_tasks)
+    completed_tasks = []
+    for task in tasks:
+        if task['completed']:
+            completed_tasks.append(task)
+
+    completed = len(completed_tasks)
     total_tasks = len(tasks)
 
-
+    print(f"Employee {name} is done with tasks({completed}/{total_tasks}):")
     for task in completed_tasks:
-        task_title = task.get('title')
-        print(f"\t{task_title}")
+        print(f"\t{task['title']}")
+
