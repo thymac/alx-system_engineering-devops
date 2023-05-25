db36bf0a237e5a9f0e29d1141149d1e255da12a5
thymac <thywillberdone@gmail.com>
2023-05-25 13:21:59 -0700

    revised

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,38 +1,41 @@
 #!/usr/bin/python3
 """
-Python script that, using a given REST API,
-returns information about an employee's TODO list progress.
+Python script that, using a REST API, retrieves and displays information
+about the TODO list progress of a given employee ID.
 """
+
 import requests
 from sys import argv
+
 
 if __name__ == "__main__":
     if len(argv) < 2:
         exit()
 
     employee_id = argv[1]
-    base_url = 'https://jsonplaceholder.typicode.com'
+    base_url = "https://jsonplaceholder.typicode.com"
 
-    # Get employee name
-    user_url = f'{base_url}/users/{employee_id}'
-    user_response = requests.get(user_url)
-    user_data = user_response.json()
-    employee_name = user_data.get("name")
+    # Get the employee's information
+    user_url = f"{base_url}/users/{employee_id}"
+    response = requests.get(user_url)
+    user_data = response.json()
+    employee_username = user_data.get("username")
 
-    # Get employee's TODO list
-    todos_url = f'{base_url}/todos?userId={employee_id}'
-    todos_response = requests.get(todos_url)
-    todos_data = todos_response.json()
+    # Get the employee's TODO tasks
+    todo_url = f"{base_url}/todos?userId={employee_id}"
+    response = requests.get(todo_url)
+    todo_data = response.json()
 
-    # Count completed tasks
-    completed_tasks = [task for task in todos_data if task.get('completed')]
-    num_completed_tasks = len(completed_tasks)
-    total_num_tasks = len(todos_data)
+    # Count the number of completed tasks and total tasks
+    completed_tasks = [task for task in todo_data if task.get("completed")]
+    total_tasks = len(todo_data)
 
-    # Print progress information
-    print(f"Employee {employee_name} is done with tasks({num_completed_tasks}/{total_num_tasks}):")
+    # Display the employee TODO list progress
+    print(f"Employee {employee_username} is done with tasks "
+          f"({len(completed_tasks)}/{total_tasks}):")
 
-    # Print titles of completed tasks
+    # Display the title of completed tasks
     for task in completed_tasks:
-        print(f"\t{task.get('title')}")
+        task_title = task.get("title")
+        print(f"\t{task_title}")
 
