10fdd57b5a9dbcdd5447eb170488c863369accb3
thymac <thywillberdone@gmail.com>
2023-06-15 13:34:35 +0000

    revised

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,5 @@
 #change ULIMIT
 exec { 'Change ULIMIT':
-<<<<<<< HEAD
   command  => 'echo "ULIMIT=\"-n 25000\"" > /etc/default/nginx && sudo service nginx restart',
   provider => shell,
-=======
-  command => 'sudo sed -i "s/^ULIMIT=\"-n = 15\"/ULIMIT=\"-n 2500\"/" /etc/default/nginx && sudo service nginx restart',
-  path    => shell,
->>>>>>> b2af7950bd0996114633e7c0e77871eab2dd6ab9
 }
