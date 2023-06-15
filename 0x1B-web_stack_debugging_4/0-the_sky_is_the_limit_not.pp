8b27b64ae3b1ea62bf87f5d146d2df3e66d42d00
thymac <thywillberdone@gmail.com>
2023-06-15 13:17:14 +0000

    revised

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,7 @@
+# Fixing the issue of failed requests by modifying the ULIMIT in the /etc/default/nginx file.
+
+exec { 'Change ULIMIT':
+  command => 'sudo sed -i "s/^ULIMIT=\"-n = 15\"/ULIMIT=\"-n 2500\"/" /etc/default/nginx && sudo service nginx restart',
+  path    => '/bin:/usr/bin:/usr/sbin:/sbin',
+}
+
